9bb094ba86f52ef9dfabe45506a21255bd353a0d
aboutony <138366071+aboutony@users.noreply.github.com>
2026-02-22 13:42:26 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "system-ui", padding: 24, lineHeight: 1.5 }}>
+      <h1>Rum-1 is running</h1>
+      <p>This server hosts the Telegram webhook for your bot.</p>
+      <p>If you can see this page, deployment is OK.</p>
+    </main>
+  );
+}
